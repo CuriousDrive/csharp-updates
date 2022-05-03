54142ee8ca026370cc3ed09f3881f6091f3f6c42
Curious Drive <curiousdrive@gmail.com>
2022-05-03 17:35:48 -0400

    Getting Started!!

---
+++ RawStringLitetrals/Program.cs
@@ -0,0 +1,9 @@
+﻿int high = 70;
+int low = 55;
+
+string weatherMessage = "\"Temp Ranges\" : {\n" +
+                        $"    \"High\" : {high}\n" +
+                        $"    \"Low\" : {low}\n" +
+                        "}";
+
+Console.WriteLine(weatherMessage);
