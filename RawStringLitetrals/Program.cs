d1bcd41564a9e566184beded1eac1f2c778bdc3c
Curious Drive <curiousdrive@gmail.com>
2022-05-03 17:43:18 -0400

    Done!!!

--- RawStringLitetrals/Program.cs
+++ RawStringLitetrals/Program.cs
@@ -1,9 +1,18 @@
-﻿int high = 70;
-int low = 55;
+﻿
+// string weatherMessage = "\"TemperatureRanges\":\n" + 
+//                         "{\n" +
+//                         $"  \"High\" : {high}\n" +
+//                         $"  \"Low\" : {low}\n" +
+//                         "}";
 
-string weatherMessage = "\"Temp Ranges\" : {\n" +
-                        $"    \"High\" : {high}\n" +
-                        $"    \"Low\" : {low}\n" +
-                        "}";
+int high = 70;
+int low = 55;
+string weatherMessage = $$"""
+                        "TemperatureRanges":
+                        {
+                            "High" : {{high}}
+                            "Low" : {{low}}
+                        }
+                        """;
 
 Console.WriteLine(weatherMessage);
