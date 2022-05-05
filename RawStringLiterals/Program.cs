f305e4ec9d60fb9618d688c645c8175c64cfec9a
Curious Drive <curiousdrive@gmail.com>
2022-05-05 14:26:25 -0400

    Done!!!

---
+++ RawStringLiterals/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// string weatherMessage = "\"TemperatureRanges\":\n" + 
+//                         "{\n" +
+//                         $"  \"High\" : {high}\n" +
+//                         $"  \"Low\" : {low}\n" +
+//                         "}";
+
+int high = 70;
+int low = 55;
+string weatherMessage = $$"""
+                        "TemperatureRanges":
+                        {
+                          "High" : {{high}}
+                          "Low" : {{low}}
+                        }
+                        """;
+Console.WriteLine(weatherMessage);
